93985248702ba2be56357dfd7080f9800c13b3dd
Anderson Foschiera <foschieraanderson@gmail.com>
2023-08-05 07:37:50 -0300

    Add dynamic content with external api

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+const mountElement = (elem) => {
+    const main = document.querySelector('main');
+
+    // Create elements
+    const card = document.createElement("div");
+    const cardContent = document.createElement("div");
+    const cardFooter = document.createElement("div");
+    const cardImage = document.createElement("img");
+    const cardTitle = document.createElement("h3");
+
+    // Add css classes
+    card.classList.add('card');
+    cardContent.classList.add('card-content');
+    cardFooter.classList.add('card-footer');
+
+    // Mount element
+    cardContent.appendChild(cardImage);
+    cardFooter.appendChild(cardTitle);
+    card.appendChild(cardContent);
+    card.appendChild(cardFooter);
+
+    // Add new element
+    cardImage.src = elem.image;
+    cardTitle.textContent = elem.name;
+    main.appendChild(card);
+}
+
+const getResponse = async (url) => {
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        data.results.map(elem => {
+            mountElement(elem);
+        });
+
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+};
+
+(() => {
+    const BaseURL = 'https://rickandmortyapi.com/api/character'
+    getResponse(BaseURL);
+
+})();
+
